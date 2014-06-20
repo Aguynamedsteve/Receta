0ab5e097f7dd3f9440b117adc5612ab70993b77b
Steve <steve.barnesjr@yahoo.com>
2014-06-19 20:38:27 -0700

    lite work

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Receta::Application.routes.draw do
-  get "recipes/index"
-  get "recipes/show"
+
+  get "home/index"
+  resources :recipes, only: [:index, :show]
+  
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
